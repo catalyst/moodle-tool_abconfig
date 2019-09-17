9c77b371e070f93dc19ce11518c37e59e611e09b
[Peter Burnett] <[peterburnett@catalyst-au.net]>
2019-09-17 10:28:57 +1000

    Added initial functions for experiment and condition managemenet

--- classes/experimentmanager.php
+++ classes/experimentmanager.php
@@ -27,5 +27,77 @@ namespace tool_abconfig;
 defined('MOODLE_INTERNAL') || die();
 class experiment_manager {
 
+    // ========================================EXPERIMENT FUNCTIONS================================================================
+
+    public function add_experiment($name, $shortname, $scope) {
+        global $DB;
+        // Check whether experiment already exists, if not return false
+        if ($this->experiment_exists($shortname)) {
+            return false;
+        } else {
+            $DB->insert_record('tool_abconfig_experiment', array('name' => $name, 'shortname' => $shortname, $scope => 'scope'));
+        }
+    }
+
+    public function experiment_exists($shortname){ 
+        global $DB;
+        return $DB->record_exists('tool_abconfig_experiment', array('shortname' => $shortname));
+    }
+
+    public function update_experiment($name, $shortname, $scope) {
+        //Check whether the experiment exists to be updated
+        if (!$this->experiment_exists($shortname)) {
+            return false;
+        } else {
+            $DB->update_record('tool_abconfig_experiment', array('name' => $name, 'shortname' => $shortname, 'scope' => $scope));
+        }
+    }
+
+    public function delete_experiment($shortname) {
+        global $DB;
+        // Check whether experiment exists to be deleted
+        if (!$this->experiment_exists($shortname)) {
+            return false;
+        } else {
+            $DB->delete_records('tool_abconfig_experiment', array('shortname' => $shortname));
+        }
+    }
+
+    // ===============================================CONDITION FUNCTIONS==================================================
+
+    public function condition_exists($eid, $condset) {
+        global $DB;
+        return $DB->record_exists('tool_abconfig_condition', array('experiment' => $eid, 'condset' => $condset));
+    }
+
+    public function add_condition($eid, $condset, $iplist, $commands, $value) {
+        global $DB;
+        if ($this->condition_exists($eid, $condset)) {
+            return false;
+        } else {
+            return $DB->insert_record('tool_abconfig_condition', array('experiment' => $eid, 'condset' => $condset, 'ipwhitelist' => $iplist,
+                'commands' => $commands, 'value' => $value));
+        }
+    }
+
+    public function update_condition($eid, $condset, $iplist, $commands, $value) {
+        global $DB;
+        if (!$this->condition_exists($eid, $condset)) {
+            return false;
+        } else {
+            return $DB->update_record('tool_abconfig_condition', array('experiment' => $eid, 'condset' => $condset, 'ipwhitelist' => $iplist,
+            'commands' => $commands, 'value' => $value));
+        }
+    }
+
+    public function delete_condition($eid, $condset) {
+        global $DB;
+        if (!$this->condition_exists($eid, $condset)) {
+            return false;
+        } else {
+            return $DB->delete_records('tool_abconfig_condition', array('experiment' => $eid, 'condset' => $condset));
+        } 
+    }
+
 }
 
