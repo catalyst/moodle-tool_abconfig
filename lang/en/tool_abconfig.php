4886067763a8f41245bcef21115b107fab23cf34
Brendan Heywood <brendan@catalyst-au.net>
2026-02-09 17:15:42 +1100

    Revert "Addressed CI errors and warnings."
    This reverts commit 6568b5435a835275218285febcf42fa2523ba72a.

--- lang/en/tool_abconfig.php
+++ lang/en/tool_abconfig.php
@@ -23,54 +23,67 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+$string['pluginname'] = 'A/B config';
+
+// Page Name Strings.
 $string['abconfigsettings'] = 'A/B config settings';
-$string['adminenabled'] = 'Enabled for admin';
-$string['cachedef_experiments'] = 'Cache to store experiments and conditions in.';
-$string['device'] = 'Device';
-$string['edit'] = 'Edit';
-$string['editexperimentconds'] = 'Edit experiment conditions';
+$string['manageexperimentspagename'] = 'Manage experiments';
 $string['editexperimentpagename'] = 'Edit experiment';
-$string['enabled'] = 'Enabled';
+$string['editexperimentconds'] = 'Edit experiment conditions';
+
+// Form Strings.
 $string['formaddexperiment'] = 'Add experiment';
-$string['formaddrepeat'] = 'Add condition set';
-$string['formallusers'] = 'All users';
-$string['formdeleteexperiment'] = 'Delete experiment';
-$string['formdeleterepeat'] = 'Delete';
-$string['formeditconditions'] = 'Edit conditions';
-$string['formexperimentadminenable'] = 'Enable this experiment for site admins.';
-$string['formexperimentalreadyexists'] = 'Experiment shortname already exists';
-$string['formexperimentcommands'] = 'Experiment commands';
-$string['formexperimentconds'] = 'Experiment conditions';
-$string['formexperimentcondsset'] = 'Conditions set';
-$string['formexperimentenabled'] = 'Enable experiment';
-$string['formexperimentforceurl'] = 'Force URL params';
-$string['formexperimentinfo'] = 'Experiment info';
 $string['formexperimentname'] = 'Experiment name:';
 $string['formexperimentnamereq'] = 'Experiment name required';
-$string['formexperimentscopeselect'] = 'Scope:';
 $string['formexperimentshortname'] = 'Short experiment name:';
 $string['formexperimentshortnamereq'] = 'Short experiment name required';
-$string['formexperimentusers'] = 'Users';
+$string['formexperimentscopeselect'] = 'Scope:';
+$string['formexperimentalreadyexists'] = 'Experiment shortname already exists';
+$string['formexperimentinfo'] = 'Experiment info';
+$string['formexperimentconds'] = 'Experiment conditions';
+$string['formipwhitelist'] = 'IP whitelist';
+$string['formexperimentcommands'] = 'Experiment commands';
 $string['formexperimentvalue'] = ' % value of traffic targeted';
 $string['formexperimentvalueerror'] = ' % value must be a number in range 0-100';
+$string['formexperimentusers'] = 'Users';
+$string['formexperimentcondsset'] = 'Conditions set';
 $string['formexperimentvalueexceed'] = 'Total of all condition set values must be <= 100. Currently {$a}';
+$string['formexperimentenabled'] = 'Enable experiment';
+$string['formdeleterepeat'] = 'Delete';
+$string['formaddrepeat'] = 'Add condition set';
 $string['formheader'] = 'Condition set {$a}';
-$string['formipwhitelist'] = 'IP whitelist';
 $string['formnnewconditions'] = 'New condition set';
+$string['formeditconditions'] = 'Edit conditions';
 $string['formnocommands'] = 'No commands';
 $string['formnoips'] = 'No IPs';
-$string['manageexperimentspagename'] = 'Manage experiments';
+$string['formallusers'] = 'All users';
+$string['formaddexperiment'] = 'Add experiment';
+$string['formdeleteexperiment'] = 'Delete experiment';
+$string['formexperimentforceurl'] = 'Force URL params';
+$string['formexperimentadminenable'] = 'Enable this experiment for site admins.';
+
+// Short Strings.
+$string['request'] = 'Request';
+$string['session'] = 'Session';
+$string['device'] = 'Device';
 $string['name'] = 'Experiment name';
-$string['no'] = 'No';
+$string['shortname'] = 'Short experiment name';
+$string['scope'] = 'Experiment scope';
 $string['offset'] = 'Offset';
 $string['offset_help'] = 'Offset value between 0 and 99 that will be used to rotate device hashes.';
-$string['pluginname'] = 'A/B config';
-$string['privacy:metadata'] = 'This plugin does not collect or store any user information.';
-$string['request'] = 'Request';
-$string['scope'] = 'Experiment scope';
-$string['session'] = 'Session';
-$string['settingcustommessage'] = 'Set by abconfig: {$a}';
-$string['settingsenabledesc'] = 'Enable the plugin. While this is unchecked, users will not interact with the plugin at all. Experiments can still be set.';
-$string['settingsenablename'] = 'Enable plugin';
-$string['shortname'] = 'Short experiment name';
+$string['edit'] = 'Edit';
+$string['enabled'] = 'Enabled';
 $string['yes'] = 'Yes';
+$string['no'] = 'No';
+$string['adminenabled'] = 'Enabled for admin';
+
+// Setting Strings.
+$string['settingsenablename'] = 'Enable plugin';
+$string['settingsenabledesc'] = 'Enable the plugin. While this is unchecked, users will not interact with the plugin at all. Experiments can still be set.';
+$string['settingcustommessage'] = 'Set by abconfig: {$a}';
+
+// Cache Strings.
+$string['cachedef_experiments'] = 'Cache to store experiments and conditions in.';
+
+// Privacy Strings.
+$string['privacy:metadata'] = 'This plugin does not collect or store any user information.';
