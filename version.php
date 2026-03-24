997674c7b940993f8b92e835cd5a9afddd52a305
Brendan Heywood <brendan@catalyst-au.net>
2026-03-24 22:59:05 +1100

    Updated copyright

--- version.php
+++ version.php
@@ -19,7 +19,7 @@
  *
  * @package   tool_abconfig
  * @author    Brendan Heywood <brendan@catalyst-au.net>
- * @copyright Catalyst IT
+ * @copyright 2019 Catalyst IT
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
