30d999be04ae4a2cca1164a4f5b88e7bc96a9d7f
Abhinav Gandham <abhinavgandham@catalyst-au.net>
2026-09-03 15:05:01 +1000

    MDL-87756: Fix issue where abconfig message property isn't being populated for the hook.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026011301;      // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2026011302;      // The current plugin version (Date: YYYYMMDDXX).
 $plugin->release   = 2025011302;      // Same as version.
 $plugin->requires  = 2025041400;
 $plugin->supported = [500, 501];       // Available as of Moodle 5.0 or later.
