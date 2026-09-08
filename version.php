d61c02fb3fb8dccd060f637d7e3422059c5fb358
Jay Oswald <jozzie52@gmail.com>
2026-09-08 18:33:44 +1000

    503 support

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026011301;      // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2026011302;      // The current plugin version (Date: YYYYMMDDXX).
 $plugin->release   = 2025011302;      // Same as version.
 $plugin->requires  = 2025041400;
-$plugin->supported = [500, 501];       // Available as of Moodle 5.0 or later.
+$plugin->supported = [500, 503];       // Available as of Moodle 5.0 or later.
 $plugin->component = "tool_abconfig";
 $plugin->maturity  = MATURITY_STABLE;
